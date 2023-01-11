22996bb6b1b4e93089351fbe1d256058ea2ed6e9
howCanIFind <second9000@naver.com>
2023-01-11 11:38:48 +0900

    Initial commit

---
+++ src/test/java/com/book/spring/springbootaws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.spring.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
