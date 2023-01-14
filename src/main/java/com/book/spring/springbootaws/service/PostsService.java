6091d6014bf361ee4afb52c98e2436db9dfb846e
howCanIFind <second9000@naver.com>
2023-01-15 05:06:38 +0900

    posts api controller 및 posts service 추가

---
+++ src/main/java/com/book/spring/springbootaws/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.book.spring.springbootaws.service;
+
+import com.book.spring.springbootaws.domain.posts.PostsRepository;
+import com.book.spring.springbootaws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
