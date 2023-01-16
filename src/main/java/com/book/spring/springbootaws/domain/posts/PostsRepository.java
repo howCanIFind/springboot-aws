03fbbaea7c1e5e4187d5d82e956bd0f183ef2a86
howCanIFind <second9000@naver.com>
2023-01-16 18:58:33 +0900

    수정하기 화면 작성 완료

--- src/main/java/com/book/spring/springbootaws/domain/posts/PostsRepository.java
+++ src/main/java/com/book/spring/springbootaws/domain/posts/PostsRepository.java
@@ -2,6 +2,12 @@ package com.book.spring.springbootaws.domain.posts;
 
 import com.book.spring.springbootaws.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/book/spring/springbootaws/service/PostsService.java
+++ src/main/java/com/book/spring/springbootaws/service/PostsService.java
@@ -2,12 +2,16 @@ package com.book.spring.springbootaws.service;
 
 import com.book.spring.springbootaws.domain.posts.Posts;
 import com.book.spring.springbootaws.domain.posts.PostsRepository;
+import com.book.spring.springbootaws.web.dto.PostsListResponseDto;
 import com.book.spring.springbootaws.web.dto.PostsResponseDto;
 import com.book.spring.springbootaws.web.dto.PostsSaveRequestDto;
 import com.book.spring.springbootaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
