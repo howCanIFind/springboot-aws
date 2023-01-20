72722232d79c53c4e065919b0413b4977ff1a9ec
howCanIFind <second9000@naver.com>
2023-01-20 11:17:11 +0000

    네이버 로그인 오류 해결

--- src/main/java/com/book/spring/springbootaws/web/IndexController.java
+++ src/main/java/com/book/spring/springbootaws/web/IndexController.java
@@ -22,7 +22,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
-        System.out.println("여기까지");
         model.addAttribute("posts", postsService.findAllDesc());
         if (user != null) {
             model.addAttribute("userName", user.getName());
